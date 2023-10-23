1f593b7e66f0d22657e0856497799001bd647bfb
matteac <mateoeestn2@gmail.com>
2023-10-23 18:12:43 -0300

    meta tags added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,10 @@
   export let data: any;
 </script>
 
-<svelte:head>
-  <title>Notes</title>
-</svelte:head>
-
 <main class="flex flex-col w-full items-center space-y-5 my-8">
   {#if data}
     <h1 class="text-5xl font-bold">Notes</h1>
-    <AddForm action="?/add" />
+    <AddForm  />
     <div class="flex flex-col space-y-5 w-2/3">
       {#each data.notes as { id, title, content }}
         <Note {id} {title} {content} />
